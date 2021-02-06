f329fbb9597c7c5ae3f3904cbc28da81d450b59c
Het <het.parekh@somaiya.edu>
2021-02-06 16:25:27 +0530

    'upgrade'

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,2 +1,2 @@
 // export const baseUrl = 'http://localhost:3001/'
-export const baseUrl = 'https://my-json-server.typicode.com/het-parekh/json-server-todoboard/'
+export const baseUrl='https://my-json-server.typicode.com/het-parekh/json-server-todoboard/'
